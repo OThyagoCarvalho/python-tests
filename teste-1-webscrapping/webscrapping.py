24b337be2e77853b7e4c5472e35947f4b16ccc65
Thyago Carvalho <othyagocarvalho@outlook.com>
2022-06-03 20:12:42 -0300

    add comments

--- teste-1-webscrapping/webscrapping.py
+++ teste-1-webscrapping/webscrapping.py
@@ -10,15 +10,15 @@ soup = BeautifulSoup(page.text, 'html.parser')
 
 anchor_tags_list = soup.select("p.callout>a")
 
-zipObj = ZipFile('Anexos.zip', 'w')
+zipObj = ZipFile('Anexos.zip', 'w') # opens a new a zip file in Write mode
 
-for tag in anchor_tags_list:
-    href = tag.get('href')
-    file_name = href.split('/')[-1]
-    if ("Anexo" in file_name) and (".pdf" in file_name):
+for tag in anchor_tags_list: 
+    href = tag.get('href') # gets the href content for every anchor tag
+    file_name = href.split('/')[-1]  # gets the file name from the last index of the split operation
+    if ("Anexo" in file_name) and (".pdf" in file_name): # searchs for Anexo and pdf format in file name
         file = requests.get(href)
-        open(file_name, 'wb').write(file.content)
-        zipObj.write(file_name)
+        open(file_name, 'wb').write(file.content) # writes pdf tile to system
+        zipObj.write(file_name) # writes downloaded pdf file to zip file previously created: see line 13
 
 print("Download concluído!")
 zipObj.close();
